70862575d4dc852f45f931e45989df11acb22b35
sn3w <92777884+sn3w@users.noreply.github.com>
2021-11-18 21:58:28 +0500

    First commit: Here be bugs and dragons

---
+++ bufferline.lua
@@ -0,0 +1,28 @@
+vim.opt.termguicolors = true
+require('bufferline').setup {
+  options = {
+    numbers = "none",
+    indicator_icon = '▎',
+    buffer_close_icon = '',
+    modified_icon = '●',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
+    max_name_length = 18,
+    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    tab_size = 18,
+    diagnostics = "nvim_lsp",
+    diagnostics_update_in_insert = false,
+    show_buffer_icons = true, -- disable filetype icons for buffers
+    show_buffer_close_icons = true,
+    show_close_icon = true,
+    show_tab_indicators = true,
+    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+    -- can also be a table containing 2 custom separators
+    -- [focused and unfocused]. eg: { '|', '|' }
+    separator_style = "thin",
+    enforce_regular_tabs = true,
+    always_show_bufferline = true,
+    sort_by = 'directory'
+  }
+}
---
+++ init.vim
@@ -0,0 +1,8 @@
+set nocp
+luafile $HOME/.config/nvim/settings.lua
+luafile $HOME/.config/nvim/treesitter.lua
+luafile $HOME/.config/nvim/lsp.lua
+luafile $HOME/.config/nvim/bufferline.lua
+luafile $HOME/.config/nvim/toggleterm.lua
+luafile $HOME/.config/nvim/lualine.lua
+
